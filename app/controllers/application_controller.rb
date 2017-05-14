5e3c44584166f3d54f988e969e858830972b96e8
Daniel Wood <daniel.wood@owasp.org>
2017-05-14 11:13:15 -0400

    Add name to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  # Since Devise 4.1 devise_parameter_sanitizer.for has ben deprecated - use .permit
+  # Need to also update << :name to keys: [:name]
+  def configure_permitted_parameters
+  	devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  	devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
