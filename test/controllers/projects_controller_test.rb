359849b30504d159856200af22779b90dae32cf5
Daniel Wood <daniel.wood@owasp.org>
2026-02-17 22:44:13 -0600

    feat: add Highcharts metrics on projects index (closes #2)

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class ProjectsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index renders metrics containers" do
+    get root_url
+    assert_response :success
+
+    assert_includes @response.body, "department-chart"
+    assert_includes @response.body, "testtype-chart"
+    assert_includes @response.body, "avgcost-chart"
+  end
 end
